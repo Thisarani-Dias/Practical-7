d3ae099004a886c0e8e8b89a7d6ba4199a66dc33
Thisarani Dias <140165499+Thisarani-Dias@users.noreply.github.com>
2024-08-04 21:57:56 -0700

    Update practical9.scala

--- practical9.scala
+++ practical9.scala
@@ -2,23 +2,19 @@ package q1
 
 object practical9 {
 
-  // 1. filterEvenNumbers function
   def filterEvenNumbers(numbers: List[Int]): List[Int] = {
     numbers.filter(n => n % 2 == 0)
   }
 
-  // 2. calculateSquare function
   def calculateSquare(numbers: List[Int]): List[Int] = {
     numbers.map(n => n * n)
   }
 
-  // Helper function to check if a number is prime
   def isPrime(n: Int): Boolean = {
     if (n <= 1) false
     else !(2 until n).exists(i => n % i == 0)
   }
 
-  // 3. filterPrime function
   def filterPrime(numbers: List[Int]): List[Int] = {
     numbers.filter(n => isPrime(n))
   }
@@ -28,14 +24,14 @@ object practical9 {
   def main(Args: Array[String]): Unit = {
     val input1 = List(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
     println("Even Numbers:")
-    println(filterEvenNumbers(input1)) // Output: List(2, 4, 6, 8, 10)
+    println(filterEvenNumbers(input1)) 
 
     val input2 = List(1, 2, 3, 4, 5)
     println("Square Numbers:")
-    println(calculateSquare(input2)) // Output: List(1, 4, 9, 16, 25)
+    println(calculateSquare(input2))
 
     println("Prime Numbers:")
-    println(filterPrime(input1)) // Output: List(2, 3, 5, 7)
+    println(filterPrime(input1))
   }
 
 }
